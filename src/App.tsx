58810bfde433de5b89eedec74550098bef08f9eb
Filipe Moura <filipeasm18@gmail.com>
2022-06-24 20:17:23 -0300

    feat: moved providers to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import { Classroom } from "./pages/Classroom"
+import { ApolloProvider } from "@apollo/client"
+import { BrowserRouter } from "react-router-dom"
+
+import { client } from "./lib/apollo"
+import { Router } from "./Router"
 
 function App() {
 
   return (
-    <Classroom />
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
