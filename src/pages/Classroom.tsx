a181b558ea90fec11bfa5b8e905094df6b2227cd
Filipe Moura <filipeasm18@gmail.com>
2022-06-24 23:51:38 -0300

    style: indentation fix

--- src/pages/Classroom.tsx
+++ src/pages/Classroom.tsx
@@ -12,7 +12,8 @@ export function Classroom() {
       <main className="flex flex-1">
         { slug 
           ? <Video lessonSlug={slug} /> 
-          : <div className="flex-1" /> }
+          : <div className="flex-1" />
+        }
         <Sidebar />
       </main> 
     </div>
